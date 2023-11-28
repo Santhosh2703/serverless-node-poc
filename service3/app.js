6e64c290b256c8165696f205120c6f6d2e0f338e
Santhosh <santhosh.annamalai@resmed.co.in>
2023-11-28 19:09:45 +0530

    deploying cicd for monorepo

---
+++ service3/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const sls = require('serverless-http');
+const app = express();
+
+// Accessing environment variables
+const deployedAt = process.env.DEPLOYED_AT;
+const gitHash = process.env.GIT_HASH;
+
+console.log('Deployed At:', deployedAt);
+console.log('Git Hash:', gitHash);
+
+app.get('/', async (req, res, next) => {
+  res.status(200).send('Hi! Monorepo CI/CD with GitHub Actions - service3');
+});
+
+module.exports.server = sls(app);
